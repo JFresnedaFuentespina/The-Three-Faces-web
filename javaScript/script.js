810bca38f9aa7230b27bb895370715b23e963ab7
Uxía Formoso <uxiaformoso@paucasesnovescifp.cat>
2026-05-05 11:46:15 +0200

    Feat: js for the dark-mode button done. To do: add new colors for the dark-mode of the page

--- javaScript/script.js
+++ javaScript/script.js
@@ -8,3 +8,34 @@ navbarToggle.addEventListener('click', () => {
 
 
 });
+
+// Dark-Mode
+const themeBtn = document.getElementById('theme-toggle');
+const coinImg = document.getElementById('coin-img');
+
+//Path to images
+const humanCoin = "Img/coins/moneda_cara.png";
+const ghostCoin = "Img/coins/moneda_cruz.png";
+
+//Function to set theme
+function setTheme(theme) {
+  document.documentElement.setAttribute('data-theme', theme);
+  localStorage.setItem('theme', theme);
+
+  if(theme === 'dark'){
+    coinImg.src = ghostCoin;
+  } else {
+    coinImg.src = humanCoin;
+  }
+}
+
+// Check for saved user preference on load
+const savedTheme = localStorage.getItem('theme') || 'light';
+setTheme(savedTheme);
+
+//Toggle on click
+themeBtn.addEventListener('click', () => {
+  const currentTheme = document.documentElement.getAttribute('data-theme');
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  setTheme(newTheme);
+});
